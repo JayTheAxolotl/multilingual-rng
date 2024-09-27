7ebb553df6c573151fc3c37ed4f120602016313c
AxolotlInANutshell <109018171+JayTheAxolotl@users.noreply.github.com>
2024-09-26 20:09:45 -0500

    Removed the avg debug option

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let listOfNum = []
-
-function averageFromList(list){
-    let temp = 0
-    for(i=list.length; i>0; i--){
-        temp = temp + list[i-1]
-    }
-    return temp/list.length
-}
-
 function randomBetweenInt(min, max){
     tMax = (max+1) - min
     return Math.floor(Math.random()*tMax) + min
@@ -16,8 +6,6 @@ function randomBetweenInt(min, max){
 function gen(){
     let temp = randomBetweenInt(Number(document.getElementById("min").value), Number(document.getElementById("max").value))
     document.getElementById("out").innerHTML = temp
-    listOfNum.push(temp) 
-    document.getElementById("avg").innerHTML = ["avg", averageFromList(listOfNum)]
 }
 
 gen()
